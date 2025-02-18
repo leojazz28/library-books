02b9b6a3cb66bb34180e6fb541de73cd8d7e3b07
leojazz <leonardosousa.linux@gmail.com>
2025-02-17 22:31:04 -0300

    Projeto

---
+++ BookStoreManeger/src/main/java/com/books/BookStoreManegerApplication.java
@@ -0,0 +1,13 @@
+package com.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreManegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreManegerApplication.class, args);
+	}
+
+}
